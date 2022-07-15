9522779c67d4dd01cba76bff710dffbebe616d32
Káyọ̀dé Fáníyì <101592250+milicentbystander@users.noreply.github.com>
2022-07-15 18:11:45 +0000

    Rewrote the functions to remove customized array values

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,11 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-const arrayOfNumbers = [68, -68, 27, 94, 72, -25, -51, 32, 10, 64, -94, 4, 34, -86, 90, 81, 20, -56, -91, -50];
+//const arrayOfNumbers = [68, -68, 27, 94, 72, -25, -51, 32, 10, 64, -94, 4, 34, -86, 90, 81, 20, -56, -91, -50];
 // const length = arrayOfNumbers.length;
+//let arrayOfNumbers = [];
 let sum = 0;
-function sumOfNumbers() {
+function sumOfNumbers(arrayOfNumbers) {
   arrayOfNumbers.forEach (item => {
     sum+=item;
   })
@@ -24,11 +25,11 @@ function sumOfNumbers() {
   // for(let i = 0; i < length;
   //   sum+=arrayOfNumbers[i++]);
 console.log(`The sum of the numbers in the given array is:`)
-  
   return sum;   
 }
 
    assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -58,12 +59,12 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-const celciusArray = [24, 30, 14, -8, 22, 17, -5, 25, 2, 25, 10, 10, 28, 33, 39, 28, 23, -1, 2, -4]
+//const celciusArray = [24, 30, 14, -8, 22, 17, -5, 25, 2, 25, 10, 10, 28, 33, 39, 28, 23, -1, 2, -4]
 let fahrenheitArray = []
 let fahrenheitValue;
-function celsiusToFahrenheit(celsiusArray) {
+function celsiusToFahrenheit(arrayOfNumbers) {
   
-  celciusArray.forEach (item => {
+  arrayOfNumbers.forEach (item => {
    fahrenheitValue = (item*1.8)+32;
     fahrenheitArray.push(Math.trunc(fahrenheitValue));
   })
