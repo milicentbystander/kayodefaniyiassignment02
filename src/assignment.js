45539cb317cde2086424d54cbd32c5bff4c792c9
Káyọ̀dé Fáníyì <101592250+milicentbystander@users.noreply.github.com>
2022-07-15 03:41:09 +0000

    *Wrote a function to sum  the values in a given array
    *Wrote another function to convert Celcius temperatures in an array to Fahrenheit and push these new values into a new array

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,22 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+const arrayOfNumbers = [68, -68, 27, 94, 72, -25, -51, 32, 10, 64, -94, 4, 34, -86, 90, 81, 20, -56, -91, -50];
+// const length = arrayOfNumbers.length;
+let sum = 0;
+function sumOfNumbers() {
+  arrayOfNumbers.forEach (item => {
+    sum+=item;
+  })
+  //forEach seems to execute faster!
+  // for(let i = 0; i < length;
+  //   sum+=arrayOfNumbers[i++]);
+console.log(`The sum of the numbers in the given array is:`)
+  
+  return sum;   
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+   assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -46,10 +58,29 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+const celciusArray = [24, 30, 14, -8, 22, 17, -5, 25, 2, 25, 10, 10, 28, 33, 39, 28, 23, -1, 2, -4]
+let fahrenheitArray = []
+let fahrenheitValue;
+function celsiusToFahrenheit(celsiusArray) {
+  
+  celciusArray.forEach (item => {
+   fahrenheitValue = (item*1.8)+32;
+    fahrenheitArray.push(Math.trunc(fahrenheitValue));
+  })
+
+
+  console.log(`The Fahrenheit array derived from the given Celcius array is:`)
+    
+  return fahrenheitArray;
+  
+  
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+
+
+
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+
+
 
 
 // ========================
